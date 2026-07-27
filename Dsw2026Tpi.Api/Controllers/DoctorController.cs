2cf42f1f77e3f85eda338cdde556c4fad8701ecf
martinaromeroscro <martina.romeroscro@alu.frt.utn.edu.ar>
2026-07-27 17:36:59 -0300

    Commit prueba 2

--- Dsw2026Tpi.Api/Controllers/DoctorController.cs
+++ Dsw2026Tpi.Api/Controllers/DoctorController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Dsw2026Tpi.Api.Controllers;
 
-[Route("api/doctors")]
+[Route("doctors")]
 [Authorize(Policy = Policies.AdminPolicy)]
 public class DoctorController : AppController
 {
